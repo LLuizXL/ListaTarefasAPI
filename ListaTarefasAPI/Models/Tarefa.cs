9263a8a3807d048be1167c6d74df0b8a81f03d53
Luiz G. <luiz.mwlo76@gmail.com>
2025-02-05 09:21:14 -0300

    Início + Inclusao do método Finalizar Tarefa (PATCH)

---
+++ ListaTarefasAPI/Models/Tarefa.cs
@@ -0,0 +1,16 @@
+﻿using ListaTarefasAPI.Models;
+
+namespace ListaTarefasAPI.Models
+{
+    public class Tarefa
+    {
+
+        public int TarefaId { get; set; }
+        public string Descricao { get; set; }
+
+        public bool Concluida { get; set; }
+
+        public int TipoTarefaId { get; set; }
+        public TipoTarefa? TipoTarefa { get; set; }
+    }
+}
--- ListaTarefasAPI/Program.cs
+++ ListaTarefasAPI/Program.cs
@@ -1,6 +1,12 @@
+using ListaTarefasAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<ListaTarefasContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("ListaTarefasContext")));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
